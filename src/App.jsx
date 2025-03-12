350b39fd639ed3e97ff5227b5f32311fc479e725
Drew-A <drew.a.anzalone@gmail.com>
2025-03-12 09:57:40 -0700

    render list and view details

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from 'react';
+import * as trackService from './services/trackService';
+import TrackList from './components/TrackList/TrackList';
+import TrackDetail from './components/TrackDetails/TrackDetails';
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [tracks, setTracks] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+  const handleSelect = (track) => {
+    setSelected(track)
+  }
+
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const fetchedTracks = await trackService.index();
+        if (fetchedTracks.err) {
+          throw new Error(fetchedTracks.err);
+        }
+        setTracks(fetchedTracks);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchTracks();
+  }, []);
+
+
+  return (
+    <>
+      <TrackList tracks={tracks} handleSelect={handleSelect} />
+      <TrackDetail selected={selected} />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/TrackList/TrackList.jsx
@@ -0,0 +1,30 @@
+const TrackList = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>Track List</h1>
+      <div>
+        {!props.tracks.length ? (
+          <h2>No Tracks Yet!</h2>
+        ) : (
+          <ul>
+            {props.tracks.map((track) => (
+              <li
+                key={track._id}
+                style={{ cursor: 'pointer', color: "#646CFF" }}
+                onClick={() => props.handleSelect(track)}
+              >
+                {track.title}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+
+
+};
+
+export default TrackList;
